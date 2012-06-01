dd46d10b2675ffad759e23da8d41b1129b705ab9
Jake Davis <jake.davis5989@gmail.com>
2012-05-31 21:55:59 -0700

    email integration w/ notmuch

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,6 +16,11 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; electronic mail
+(require 'notmuch)
+(setq message-kill-buffer-on-exit t)
+(setq sendmail-program "/usr/bin/msmtp")
+
 ;; disabled for speed / lack of python/haskell developing
 
 ;; Python mode
@@ -30,3 +35,15 @@
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(send-mail-function nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
