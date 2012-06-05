e5d220d7f1e2791744f349c93413780c2207e3ff
Jake Davis <jake.davis5989@gmail.com>
2012-06-04 22:16:21 -0700

    remove python-mode, notmuch, add line numbers, remove highlighting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,32 +9,31 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Emacs-starter-kit
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-ruby)
+;; emacs-starter-kit
+(defvar my-packages '(starter-kit-lisp starter-kit-bindings starter-kit-ruby)
   "A list of packages to ensure are installed at launch.")
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
 ;; electronic mail via notmuch
-(require 'notmuch)
-(setq message-kill-buffer-on-exit t)
-(setq sendmail-program "/usr/bin/msmtp")
-
-;; disabled for speed / lack of python/haskell developing
-;; Python mode
-;;(add-to-list 'load-path "/home/jake/custom/python-mode")
-;;(setq py-install-directory "/home/jake/custom/python-mode")
-;;(require 'python-mode)
+;;(require 'notmuch)
+;;(setq message-kill-buffer-on-exit t)
+;;(setq sendmail-program "/usr/bin/msmtp")
 
 ;; Haskell mode
 (load "~/custom/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
-;;(custom-set-variables
-;; '(send-mail-function nil))
-;;(custom-set-faces
-;;)
+(custom-set-variables
+ )
+(custom-set-faces
+ '(hl-line ((t (:inherit nil)))))
+
+;; line numbers
+(global-linum-mode 1)
+
+;; send both of these terrible things to hell
+(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
+(remove-hook 'prog-mode-hook 'esk-turn-on-idle-highlight-mode)  
