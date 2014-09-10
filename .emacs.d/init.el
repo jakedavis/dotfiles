1735c5902e667d628062723fd934b8d06af31656
Jake Davis <jake.davis5989@gmail.com>
2014-09-10 23:27:45 +0000

    [Emacs] Paredit, update package archives

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,16 +5,16 @@
 
 (setq x-alt-keysym 'meta)
 
-;; Packages or whatever
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+
 (package-initialize)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")))
+
 (when (not package-archive-contents)
   (package-refresh-contents))
-(defvar my-packages '(starter-kit-lisp starter-kit-bindings starter-kit-ruby))
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
 
 ;; line numbers
 (global-linum-mode 1)
@@ -23,3 +23,11 @@
 (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 (remove-hook 'prog-mode-hook 'esk-turn-on-idle-highlight-mode)  
 
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'clojure-mode-hook          #'enable-paredit-mode)
